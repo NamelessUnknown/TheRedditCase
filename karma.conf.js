12f68c0f707042a4522f525711ce8b1db7300362
Lukasz Debiec <genbolson@gmail.com>
2021-09-18 13:18:51 +0200

    project name fix, silly me

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/TheReactCase'),
+      dir: require('path').join(__dirname, './coverage/TheRedditCase'),
       subdir: '.',
       reporters: [
         { type: 'html' },
