f612476312bb10d10b3cd84e985550dfba011b8f
Lukasz Debiec <genbolson@gmail.com>
2021-09-18 12:25:52 +0200

    Basic layout implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { FeedListComponent } from './core/feed-list/feed-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedDetailsComponent } from './core/feed-details/feed-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FeedListComponent },
+  { path: 'products/:id', component: FeedDetailsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/feed-teaser/feed-teaser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-teaser',
+  templateUrl: './feed-teaser.component.html',
+  styleUrls: ['./feed-teaser.component.scss']
+})
+export class FeedTeaserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
